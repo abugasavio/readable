ce1852ade51aec6d14535c9ba2d6f5f77774ad31
Savio Abuga <savioabuga@gmail.com>
2017-11-13 15:30:34 +0300

    fix: fix default filtering

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import PostDetail from '../post/PostDetail'
 import PostListPage from './PostListPage';
 import AddPostPage  from './AddPostPage'
 
 const App = () => (
 	<div>
 		<Route path="/" exact component={PostListPage} />
+		<Route path="/post/:id" component={PostDetail} />
 		<Route path="/add-post" component={AddPostPage} />
 		<Route path="/category/:category" component={PostListPage} />
 	</div>
