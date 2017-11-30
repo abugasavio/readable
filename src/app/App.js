7af1494633d020c5f877f5b5ee47943883f11e20
Savio Abuga <savioabuga@gmail.com>
2017-11-30 09:38:35 +0300

    feat: add a not found page for post

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,6 @@ import PostDetail from '../post/PostDetail'
 import PostListPage from './PostListPage';
 import AddPostPage  from './AddPostPage'
 import EditPostPage from './EditPostPage';
-import NotFoundPage from './NotFoundPage';
 
 const App = () => (
 	<div>
@@ -13,7 +12,6 @@ const App = () => (
     <Route path="/edit-post/:id" component={EditPostPage} />
 		<Route path="/add-post" component={AddPostPage} />
     <Route path="/category/:category" component={PostListPage} />
-    <Route path="/not-found" component={NotFoundPage} />
 	</div>
 )
 
--- src/post/PostDetail.js
+++ src/post/PostDetail.js
@@ -22,25 +22,23 @@ class PageDetail extends Component {
     const { boundFetchPost, boundFetchComments, match } = this.props;
     boundFetchPost(match.params.id)
       .then(() => boundFetchComments(match.params.id))
-      .catch(() => this.props.history.push('/not-found'));
-
   }
 
   onDeletePost = e => {
     e.preventDefault();
     const { match, boundDeletePost } = this.props;
-    boundDeletePost(match.params.id).then(this.props.history.push('/'))
+    boundDeletePost(match.params.id).then(this.props.history.push('/'));
   };
 
-  onClickVoteUpButton = (e) => {
+  onClickVoteUpButton = e => {
     const { boundVoteUpPost, match, boundFetchPost } = this.props;
-    boundVoteUpPost(match.params.id).then(() => boundFetchPost(match.params.id))
-  }
+    boundVoteUpPost(match.params.id).then(() => boundFetchPost(match.params.id));
+  };
 
   onClickVoteDownButton = () => {
     const { boundVoteDownPost, match, boundFetchPost } = this.props;
-    boundVoteDownPost(match.params.id).then(() => boundFetchPost(match.params.id))
-  }
+    boundVoteDownPost(match.params.id).then(() => boundFetchPost(match.params.id));
+  };
 
   closeModal = () => {
     this.setState({
@@ -57,60 +55,75 @@ class PageDetail extends Component {
   render() {
     const { modalOpen } = this.state;
     const postId = this.props.match.params.id;
+    console.log(this.props.post);
     return (
       <Layout>
-        <Container text style={{ marginBottom: '20px'}}>
-          <PageHeader icon='book' title={this.props.post.title} />
-          <Segment clearing basic>
-            <Button.Group floated="right">
-              <Button color='pink' onClick={this.onClickVoteUpButton}><Icon name='thumbs outline up'/></Button>
-              <Button color='pink' onClick={this.onClickVoteDownButton}><Icon name='thumbs outline down'/></Button>
-              <Button color="pink">
-                <Icon name="edit" />
-                <Link to={`/edit-post/${postId}`} role={Button}>
-                  Edit Post
-                </Link>
-              </Button>
-              <Modal
-                open={modalOpen}
-                onClose={this.closeModal}
-                trigger={
-                  <Button color="pink" onClick={this.openModal}>
-                    <Icon name="remove circle" />Delete Post
+        <Container text style={{ marginBottom: '20px' }}>
+          {!this.props.post.id && (
+            <Segment massive raised>
+              <Header>404</Header>
+              <Header>Page Not Found</Header>
+            </Segment>
+          )}
+          {this.props.post.id && (
+            <div>
+              <PageHeader icon="book" title={this.props.post.title} />
+              <Segment clearing basic>
+                <Button.Group floated="right">
+                  <Button color="pink" onClick={this.onClickVoteUpButton}>
+                    <Icon name="thumbs outline up" />
                   </Button>
-                }
-                basic
-                size="small"
-              >
-                <Header icon="remove" content="Delete Post" />
-                <Modal.Content>
-                  <p>Are you sure you want to delete this post?</p>
-                </Modal.Content>
-                <Modal.Actions>
-                  <Button basic color="red" inverted onClick={this.closeModal}>
-                    <Icon name="remove" /> No
+                  <Button color="pink" onClick={this.onClickVoteDownButton}>
+                    <Icon name="thumbs outline down" />
                   </Button>
-                  <Button color="green" inverted onClick={this.onDeletePost}>
-                    <Icon name="checkmark" /> Yes
+                  <Button color="pink">
+                    <Icon name="edit" />
+                    <Link to={`/edit-post/${postId}`} role={Button}>
+                      Edit Post
+                    </Link>
                   </Button>
-                </Modal.Actions>
-              </Modal>
-            </Button.Group>
-          </Segment>
-          <Divider />
-          <Segment clearing basic size='massive'>
-            <Header as="h1" floated="left">
-              <Header.Subheader style={{ paddingTop: '2px' }}>Votes Received: {this.props.post.voteScore}</Header.Subheader>
-            </Header>
-          </Segment>
-          <p>{this.props.post.body}</p>
-          <Comment.Group size='big'>
-            <Header as="h5" dividing>
-              Comments
-            </Header>
-            {this.props.comments.map(comment => <CommentBlock key={comment.id} {...comment} />)}
-            <AddCommentForm submit={createComment} postId={this.props.match.params.id} />
-          </Comment.Group>
+                  <Modal
+                    open={modalOpen}
+                    onClose={this.closeModal}
+                    trigger={
+                      <Button color="pink" onClick={this.openModal}>
+                        <Icon name="remove circle" />Delete Post
+                      </Button>
+                    }
+                    basic
+                    size="small"
+                  >
+                    <Header icon="remove" content="Delete Post" />
+                    <Modal.Content>
+                      <p>Are you sure you want to delete this post?</p>
+                    </Modal.Content>
+                    <Modal.Actions>
+                      <Button basic color="red" inverted onClick={this.closeModal}>
+                        <Icon name="remove" /> No
+                      </Button>
+                      <Button color="green" inverted onClick={this.onDeletePost}>
+                        <Icon name="checkmark" /> Yes
+                      </Button>
+                    </Modal.Actions>
+                  </Modal>
+                </Button.Group>
+              </Segment>
+              <Divider />
+              <Segment clearing basic size="massive">
+                <Header as="h1" floated="left">
+                  <Header.Subheader style={{ paddingTop: '2px' }}>Votes Received: {this.props.post.voteScore}</Header.Subheader>
+                </Header>
+              </Segment>
+              <p>{this.props.post.body}</p>
+              <Comment.Group size="big">
+                <Header as="h5" dividing>
+                  Comments
+                </Header>
+                {this.props.comments.map(comment => <CommentBlock key={comment.id} {...comment} />)}
+                <AddCommentForm submit={createComment} postId={this.props.match.params.id} />
+              </Comment.Group>
+            </div>
+          )}
         </Container>
       </Layout>
     );
@@ -121,17 +134,17 @@ PageDetail.propTypes = {
   boundFetchPost: PropTypes.func.isRequired,
   boundFetchComments: PropTypes.func.isRequired,
   boundDeletePost: PropTypes.func.isRequired,
-  history: PropTypes.object.isRequired,  // eslint-disable-line react/forbid-prop-types
+  history: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   comments: PropTypes.objectOf(PropTypes.objects).isRequired,
   post: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   match: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   boundVoteDownPost: PropTypes.func.isRequired,
-  boundVoteUpPost: PropTypes.func.isRequired
+  boundVoteUpPost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   post: state.posts.currentPost,
-  comments: orderBy(values(state.comments), comment => comment.voteScore, 'desc')
+  comments: orderBy(values(state.comments), comment => comment.voteScore, 'desc'),
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -139,7 +152,7 @@ const mapDispatchToProps = dispatch => ({
   boundFetchComments: id => dispatch(fetchComments(id)),
   boundDeletePost: id => dispatch(deletePost(id)),
   boundVoteUpPost: id => dispatch(voteUpPost(id)),
-  boundVoteDownPost: id => dispatch(voteDownPost(id))
+  boundVoteDownPost: id => dispatch(voteDownPost(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PageDetail);
